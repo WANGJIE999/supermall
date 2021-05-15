abd8d1eff64f855247031576b271a5e1baaf841e
WANGJIE999 <1005989940@qq.com>
2021-05-15 14:37:19 +0800

    初始化项目

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,12 +3,6 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-
-    <van-swipe :autoplay="3000">
-      <van-swipe-item v-for="(image, index) in images" :key="index">
-        <img v-lazy="image" />
-      </van-swipe-item>
-    </van-swipe>
   </div>
 </template>
 
@@ -18,14 +12,7 @@ import NavBar from "components/common/NavBar";
 export default {
   name: "",
   data() {
-    return {
-      images: [
-        "assets/images/home/index_ad1.jpg",
-        "assets/images/home/index_ad2.jpg",
-        "assets/images/home/index_ad3.jpg",
-        "assets/images/home/index_ad4.jpg",
-      ],
-    };
+    return {};
   },
   components: { NavBar },
   methods: {},
@@ -42,16 +29,5 @@ export default {
 .home-nav {
   background-color: rgb(238, 143, 159);
   color: #fff;
-}
-.my-swipe img {
-  width: 375px;
-  height: 177px;
-}
-.my-swipe .van-swipe-item {
-  color: #fff;
-  font-size: 20px;
-  line-height: 150px;
-  text-align: center;
-  background-color: #39a9ed;
 }
 </style>
--- src/views/special/Special.vue
+++ src/views/special/Special.vue
@@ -1,18 +1,103 @@
 <template>
-  <div>专题</div>
+  <div class="special">
+    <div class="loginform">
+      <img src="~assets/images/home/index_ad1.jpg" alt="" />
+      <div class="min">
+        <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
+          <FormItem prop="user">
+            <Input type="text" v-model="formInline.user" placeholder="Username">
+              <Icon type="ios-person-outline" slot="prepend"></Icon>
+            </Input>
+          </FormItem>
+          <br />
+          <FormItem prop="password">
+            <Input
+              type="password"
+              v-model="formInline.password"
+              placeholder="Password"
+            >
+              <Icon type="ios-lock-outline" slot="prepend"></Icon>
+            </Input> </FormItem
+          ><br />
+          <FormItem class="dddd">
+            <Button type="primary" @click="handleSubmit('formInline')"
+              >Signin</Button
+            >
+          </FormItem>
+        </Form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "",
   data() {
-    return {};
+    return {
+      formInline: {
+        user: "",
+        password: "",
+      },
+      ruleInline: {
+        user: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur",
+          },
+          {
+            type: "string",
+            min: 6,
+            message: "The password length cannot be less than 6 bits",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
   },
   components: {},
   mounted() {},
-  methods: {},
+  methods: {
+    handleSubmit(name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          this.$Message.success("Success!");
+        } else {
+          this.$Message.error("Fail!");
+        }
+      });
+    },
+  },
 };
 </script>
 
 <style>
+.special {
+  margin: 0 auto;
+}
+.loginform {
+  margin: 200px auto;
+  width: 530px;
+  height: 300px;
+  border: 1px solid #bfa;
+  /* background-image: url("~assets/images/home/index_ad1.jpg"); */
+}
+.min {
+  width: 200px;
+  height: 120px;
+  margin: 50px auto;
+}
+.loginform img {
+  position: absolute;
+  width: 530px;
+  height: 300px;
+}
 </style>
