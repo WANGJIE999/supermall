42e187fca2a8cc1beaaad52e2112ad98aa2a240b
WANGJIE999 <1005989940@qq.com>
2021-05-19 23:22:51 +0800

    初始化项目

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -3,6 +3,22 @@
     <nav-bar class="profilr-nav">
       <div slot="center">我的</div>
     </nav-bar>
+    <div>
+      <div class="login_box">
+        <el-form size="mini" class="login_form">
+          <el-form-item>
+            <el-input prefix-icon="iconfont icon-user"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+          </el-form-item>
+          <el-form-item class="login_but">
+            <el-button type="primary">登录</el-button>
+            <el-button type="info">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,8 +36,32 @@ export default {
 </script>
 
 <style>
+#profile {
+  width: 100%;
+  height: 667px;
+  background-color: #4b6584;
+}
 .profilr-nav {
   background-color: rgb(238, 143, 159);
   color: #fff;
+}
+.login_box {
+  width: 300px;
+  height: 200px;
+  background-color: #fff;
+  margin: 150px auto;
+  position: relative;
+}
+.login_form {
+  position: absolute;
+  bottom: 25px;
+  width: 100%;
+  padding: 0px 10px;
+}
+.login_but {
+  /* 弹性盒子 */
+  display: flex;
+  /* 居右对其 */
+  justify-content: flex-end;
 }
 </style>
--- src/views/shopcart/Shopcart.vue
+++ src/views/shopcart/Shopcart.vue
@@ -2,22 +2,7 @@
   <div id="shopcart">
     <nav-bar class="shopcart-nav"><div slot="center">购物车</div></nav-bar>
 
-    <div class="block">
-      <div class="login_box">
-        <el-form size="mini" class="login_form">
-          <el-form-item>
-            <el-input prefix-icon="iconfont icon-user"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-input prefix-icon="iconfont icon-3702mima"></el-input>
-          </el-form-item>
-          <el-form-item class="login_but">
-            <el-button type="primary">登录</el-button>
-            <el-button type="info">重置</el-button>
-          </el-form-item>
-        </el-form>
-      </div>
-    </div>
+    <div class="block"></div>
   </div>
 </template>
 
@@ -35,32 +20,8 @@ export default {
 </script>
 
 <style>
-#shopcart {
-  width: 100%;
-  height: 667px;
-  background-color: #4b6584;
-}
 .shopcart-nav {
   background-color: rgb(238, 143, 159);
   color: #fff;
-}
-.login_box {
-  width: 300px;
-  height: 200px;
-  background-color: #fff;
-  margin: 150px auto;
-  position: relative;
-}
-.login_form {
-  position: absolute;
-  bottom: 25px;
-  width: 100%;
-  padding: 0px 10px;
-}
-.login_but {
-  /* 弹性盒子 */
-  display: flex;
-  /* 居右对其 */
-  justify-content: flex-end;
 }
 </style>
